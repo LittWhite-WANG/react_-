e3196bf1bc1b76fb05b8d77c3cd72297f69b26b8
wjj <wjj7790133@163.com>
2023-03-17 19:44:56 +0800

    init app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// 修改后
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
+
+reportWebVitals();
